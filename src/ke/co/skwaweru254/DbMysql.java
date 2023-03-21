6f2e88b2f6b7edb7fa41bbd78963c9bad2781ff1
skwaweru254 <skwaweru254@gmail.com>
2023-03-21 13:11:03 +0300

    created a mysqlConnect method with connection settings

--- src/ke/co/skwaweru254/DbMysql.java
+++ src/ke/co/skwaweru254/DbMysql.java
@@ -2,11 +2,38 @@ package ke.co.skwaweru254;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class DbMysql {
-    public static void main(String[] args) {
+
+
+
+    public static void main(String[] args){
+        mysqlConnect();
 
     }
+
+
+    public static void mysqlConnect() {
+
+        //the old com.mysql.jdbc.Driver class name was deprecated
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        //?autoReconnect=true&useSSL=false will disable ssl and suppress ssl
+        String connection = "jdbc:mysql://localhost:3306/dbMSpace?autoReconnect=true&useSSL=false";
+        String user = "root";
+        String password = "Mspace54#";
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(connection, user, password);
+            System.out.println("Successfully connected to database: " +con);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Couldn't load driver.");
+        } catch (SQLException e) {
+            System.err.println("Couldn't connect to database.");
+        }
+    }
+
+
+
 }
