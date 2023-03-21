a278ecae1a616154ed25616802fcd99c89c913df
skwaweru254 <skwaweru254@gmail.com>
2023-03-21 12:01:13 +0300

    created a main method inside DbMysql.java

---
+++ src/ke/co/skwaweru254/DbMysql.java
@@ -0,0 +1,12 @@
+package ke.co.skwaweru254;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DbMysql {
+    public static void main(String[] args) {
+
+    }
+}
