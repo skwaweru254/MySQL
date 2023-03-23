272abd7f7e8127c855eae37bd45949df66de985a
skwaweru254 <skwaweru254@gmail.com>
2023-03-23 10:22:43 +0300

    created a closeConnection method

--- src/ke/co/skwaweru254/DbMysql.java
+++ src/ke/co/skwaweru254/DbMysql.java
@@ -6,34 +6,55 @@ import java.sql.SQLException;
 
 public class DbMysql {
 
+    //?autoReconnect=true&useSSL=false will disable ssl and suppress ssl
+    private static final String connection = "jdbc:mysql://localhost:3306/dbMSpace?autoReconnect=true&useSSL=false";
+
+    //the old com.mysql.jdbc.Driver class name was deprecated
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+    private static final String user = "root";
+
+    private static final String password = "Mspace54#";
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         mysqlConnect();
+        closeConnection();
 
     }
 
 
     public static void mysqlConnect() {
 
-        //the old com.mysql.jdbc.Driver class name was deprecated
-        String driver = "com.mysql.cj.jdbc.Driver";
-
-        //?autoReconnect=true&useSSL=false will disable ssl and suppress ssl
-        String connection = "jdbc:mysql://localhost:3306/dbMSpace?autoReconnect=true&useSSL=false";
-        String user = "root";
-        String password = "Mspace54#";
         try {
             Class.forName(driver);
             Connection con = DriverManager.getConnection(connection, user, password);
-            System.out.println("Successfully connected to database: " +con);
+            System.out.println("Successfully connected to database: " + con + "\n");
         } catch (ClassNotFoundException e) {
-            System.err.println("Couldn't load driver.");
+            System.err.println("Couldn't load driver.\n");
         } catch (SQLException e) {
-            System.err.println("Couldn't connect to database.");
+            System.err.println("Couldn't connect to database.\n");
         }
     }
 
+    public static void closeConnection() {
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(connection, user, password);
+            if (!con.isClosed()) {
+                con.close();
+                System.out.println("Database closed successfully.\n");
+            }
+        } catch (NullPointerException e) {
+            System.err.println("Couldn't load driver.\n");
+        } catch (SQLException e) {
+            System.err.println("Couldn't close database.\n");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Couldn't load class.\n");
+            throw new RuntimeException(e);
+
+        }
+    }
 
 
 }
